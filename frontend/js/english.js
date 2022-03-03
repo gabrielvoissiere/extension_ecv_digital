e599c001c76a7307b6dae949b602530b6af9b68d
gabrielvoissiere <gabvoissiere.pro@yahoo.com>
2022-03-03 18:39:29 +0100

    tada

---
+++ frontend/js/english.js
@@ -0,0 +1,387 @@
+const words = [{
+    theme: "Internship",
+    mot: "Estimate",
+    translation: "Devis",
+    definition: "A detailed and estimated statement of work to be done, which constitutes a preliminary project and not a formal commitment",
+    exemple: "The mechanic gave me an estimate before he fixed my car.",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Traineeship",
+    translation: "Stage ",
+    definition: "It’s a work placement where a student gains experience of the working world",
+    exemple: "At the end of my studies I have to do a traineeship in a company to get experience",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Competition",
+    translation: "Concurrence ",
+    definition: "A competition is the rivalry of interests between several people pursuing the same goal",
+    exemple: "The competition between these two tennis players is intense",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Marketing analysis",
+    translation: "Analyse marketing ",
+    definition: "Marketing analysis is the very first step in the marketing development of a company",
+    exemple: "To start the business we had to do a marketing analysis to position ourselves in the market",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Professional development",
+    translation: "Evolution pro",
+    definition: "Professional development is the learning process that allows an employee or a job seeker to acquire knowledge",
+    exemple: "My professional development has been significant since I found this new job",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Technological breakthrough",
+    translation: "Avancée technologique",
+    definition: "It literally means a technology that has broken through some kind of obstacle, barrier, or may indicate a sudden advance",
+    exemple: "Society has significantly changed due to technological breakthroughs since the year 2000",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Artificial intelligence",
+    translation: "Intelligence artificielle",
+    definition: "It means the theory and development of computer systems able to perform tasks normally requiring human intelligence, such as visual perception, speech recognition, decision-making, and translation between languages",
+    exemple: "Boston Dynamics works on artificial intelligence to facilitate our lives",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Be aware of",
+    translation: "Se tenir au courant",
+    definition: "It means that you know what’s happening: you know if a fire breaks out in your city by watching TV, social networks…",
+    exemple: "Be aware of the news, it’s important nowadays",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Connected devices",
+    translation: "Objets connectés",
+    definition: "A connected device is a device that is linked to the internet and can interact with others from afar",
+    exemple: "We all have connected devices like smartphones or computers",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "World’s digitalization",
+    translation: "Digitalisation du monde",
+    definition: "A connected device is a device that is linked to the internet and can interact with others from afar",
+    exemple: "Digitalization is the integration of digital technologies into everyday life",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Graphical charter",
+    translation: "Charte graphique",
+    definition: "The expression graphic charter corresponds to all the elements that define the visual identity of a brand, an organization or a company",
+    exemple: "I chose a color gradient for the logo colors",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Kerning",
+    translation: "Crénage",
+    definition: "Is the spacing between the letters",
+    exemple: "It’s necessary to put  kerning to help legibility",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Scale",
+    translation: "Echelle",
+    definition: "The scale bar is a set of straight lines representing the numerical size",
+    exemple: "Changing the scale of my square modifies its size",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Convey emotions",
+    translation: "Convey emotions",
+    definition: "To communicate feeling to others",
+    exemple: "I want to convey emotion through my video editing",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Sketch",
+    translation: "Croquis",
+    definition: "A sketch is a drawing done quickly, freehand, without looking for details in order to bring out in broad strokes, the essence of the subject",
+    exemple: "I made a sketch of the idea that I had",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "Follow trends",
+    translation: "Suivre les tendances",
+    definition: "To follow a trend is to analyze the orientation of something which is evolving",
+    exemple: "You have to follow the trends if you want to be in the game",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "Support the clients",
+    translation: "Accompagner les clients",
+    definition: "Supporting clients involves giving help and advice to customers",
+    exemple: "The aim of my job is to support the clients in the best way possible",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "Targeted advertising",
+    translation: "Publicité ciblée",
+    definition: "Supporting clients involves giving help and advice to customers",
+    exemple: "Targeted advertising is the most cost-effective means of communication",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "KPI",
+    translation: "Key performance indicator",
+    definition: "Data analysis is essential to know if your website is working well",
+    exemple: "KPIs allow a fact based analysis of data which facilitates taking decisions",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "Trademarks",
+    translation: "Marques",
+    definition: "Trademarks allow powerful visual communication",
+    exemple: "Trademarks are symbols and or words used to symbolize a company or product",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Commercial strategy",
+    translation: "Stratégie commerciale",
+    definition: "The commercial strategy consists in defining and implementing coordinated marketing and commercial means in order to achieve the objectives",
+    exemple: "I need to develop a commercial strategy to launch my company",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Visual identity",
+    translation: "Identité visuelle",
+    definition: "The visual identity is a set of coherent visual elements that allow identification through the use of different means of communication",
+    exemple: "I need to define a visual identity for my advertising campaign to be perfect",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Job interview",
+    translation: "Entretien d’embauche",
+    definition: "The purpose of the job interview is to verify how and why the candidate's skills are suitable for the position to be filled",
+    exemple: "I had a job interview before being accepted for my new job",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Daily meeting",
+    translation: "Points quotidiens",
+    definition: "A meeting which brings employees together for a company update",
+    exemple: "Every morning we do a daily review with the employees of my company to work in the best conditions",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Internship",
+    mot: "Remote working",
+    translation: "Faire du télétravail",
+    definition: "Professional activity carried out at a distance, through to digital tools",
+    exemple: "During the lockdown, I worked remotely to avoid traveling to my company",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Non-Fongible Token",
+    translation: "Jeton non Fongible (NFT)",
+    definition: "A unique digital identifier that cannot be copied, substituted or subdivided, that is recorded in a blockchain, and that is used to certify authenticity and ownership",
+    exemple: "The non-fongible token is used in cryptocurrency to make it secure",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Numerical law",
+    translation: "Loi du numérique",
+    definition: "Take the example of ARCOM. It's an organization that regulates the net, limiting and punishing illegal streaming sites, etc",
+    exemple: "The numerical world is changing day by day, meaning new laws or new interpretations of existing laws",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Cyberbullying",
+    translation: "Cyber harcèlemente",
+    definition: "Cyberbullying includes sending, posting, or sharing negative, harmful, false, or mean content about someone else",
+    exemple: "Online bullying is increasing due to Covid-19. Don’t do it !",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Digitized information",
+    translation: "Information digitalisée",
+    definition: "Digitalization is the process of converting pictures, sound, or information into a form that a computer can easily read",
+    exemple: "Information has become incredibly digitized in the last decade",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Digital Culture",
+    mot: "Lack of data",
+    translation: "Manque de données",
+    definition: "A lack of data is a lack of information.  Such as text, numbers,  multimedia images, audio, or videos",
+    exemple: "Our company lacks the data to do the brainstorming for the sales plan of our new product",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Gradient",
+    translation: "Dégradé",
+    definition: "A gradient is a gradual change in color from one to another",
+    exemple: "I have chosen a gradient for the color of the logo",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Grid",
+    translation: "Grille",
+    definition: "A typographic grid is a two-dimensional structure, composed of a series of intersecting vertical and horizontal axes",
+    exemple: "I use a grid on illustrator to find my way",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "User flow",
+    translation: "Flux utilisateurs",
+    definition: "A model is a prototype, a sketch of a website, or an application",
+    exemple: "I created a model for the design of the mobile application",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Graphism",
+    mot: "Typeface",
+    translation: "Caractère typographique",
+    definition: "A typeface is a design for a set of printer or display fonts, each for a set of characters, in a number of specific sizes",
+    exemple: "The typeface of this book is rounded",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "Knowledge",
+    translation: "Connaissance",
+    definition: "Knowledge is facts, information, and skills acquired through experience or educations",
+    exemple: "Thanks to my school, I acquired a lot of knowledge",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "End-user",
+    translation: "Utilisateur",
+    definition: "End-user is the person who actually uses a particular product",
+    exemple: "The software can be modified to suit the particular needs of the end-user",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Marketing",
+    mot: "Engagement Rate",
+    translation: "Taux d’engagement",
+    definition: "Engagement rates are metrics that track how actively involved with your content your audience is",
+    exemple: "The engagement rate is useful to improve your future content",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Audiovisuelle",
+    mot: "Prop",
+    translation: "Accessoire",
+    definition: "An object used on stage or screen by actors during a performance or screen production",
+    exemple: "Get the props ready for the next scene",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Audiovisuelle",
+    mot: "Off screen",
+    translation: "Hors champs",
+    definition: "Out of sight of the motion picture or television viewer a shot fired off-screen",
+    exemple: "The shot moves away from the battle and we hear the death scene which happens off-screen",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Audiovisuelle",
+    mot: "Lighting",
+    translation: "Eclairage",
+    definition: "Film lighting refers to the direction, quality, source, or colour of light",
+    exemple: "I want the litghting to highlight the face of Richard",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Audiovisuelle",
+    mot: "Short film",
+    translation: "Court-métrage",
+    definition: "An original motion picture that has running time of 40 minutes or less, including all credits",
+    exemple: " Make a short film about your experiences at school",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, {
+    theme: "Audiovisuelle",
+    mot: "Framing",
+    translation: "Cadrage",
+    definition: "In visual arts and particularly cinematography, framing is the presentation of visual elements in an image, especially the placement of the subject in relation to other objects",
+    exemple: "The framing needs to be improved",
+    img_url: "https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif",
+    audio_url: "./url/audio.mp3"
+}, 
+]
+
+let date = new Date()
+let fullDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+
+const chooseNewWord = () => {
+    const random = Math.floor(Math.random() * words.length)
+    const selected = words[random]
+
+    if (JSON.parse(localStorage.getItem("check")).mot === selected.mot) {
+        chooseNewWord()
+        return
+    }
+
+    let date = new Date()
+    let fullDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+
+    let check = {
+        nb: random,
+        date: fullDate,
+        mot: selected.mot
+    }
+
+    localStorage.setItem("check", JSON.stringify(check))
+
+    document.getElementById("theme").innerHTML = selected.theme
+    document.getElementById("wordOfTheDay").innerHTML = selected.mot
+    document.getElementById("french").innerHTML = selected.translation
+    document.getElementById("def").innerHTML = selected.definition
+    document.getElementById("exSentence").innerHTML = selected.exemple
+    document.getElementById("wimg").setAttribute("src", selected.img_url)
+}
+
+const chooseWord = () => {
+    const selected = words[JSON.parse(localStorage.getItem("check")).nb]
+
+    document.getElementById("theme").innerHTML = selected.theme
+    document.getElementById("wordOfTheDay").innerHTML = selected.mot
+    document.getElementById("french").innerHTML = selected.translation
+    document.getElementById("def").innerHTML = selected.definition
+    document.getElementById("exSentence").innerHTML = selected.exemple
+    document.getElementById("wimg").setAttribute("src", selected.img_url)
+}
+
+// check day condition
+if (fullDate !== JSON.parse(localStorage.getItem("check")).date) {
+    chooseNewWord()
+} else {
+    chooseWord()
+}
